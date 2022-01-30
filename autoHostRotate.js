ec087136d59b73b4c664334a6f80acd412af6ddc
dragongoose <tylerjseverino@gmail.com>
2022-01-29 21:25:50 -0500

    Initial Commit

---
+++ autoHostRotate.js
@@ -0,0 +1,101 @@
+class autoHostRotate {
+    constructor(client, name, pass, chalk) {
+        const run = async () => {
+
+            const color = ['red', 'green', 'blue', 'magenta', 'cyan', 'gray', 'orange'];
+            const randomnum = Math.floor(Math.random() * color.length);
+
+            let message = chalk[color[randomnum]](`[*]  ${name} >`)
+            let success = chalk.green(`[!]  ${name} >`)
+            let warn = chalk.yellow(`[!]  ${name} >`)
+            let danger = chalk.red(`[!] ${name} >`)
+
+            const exec = require('child_process').exec
+            const os = require("os");
+            const platform = os.platform();
+
+            this.players = [];
+
+            console.log(`${message} Making a Auto Host Rotate lobby`)
+
+            //Create a lobbu!
+            this.channel = await client.createLobby(name);
+            this.lobby = this.channel.lobby;
+
+            if (pass) {
+                //generate random password
+                this.password = Math.random().toString(36).substring(8);
+                await this.lobby.setPassword(this.password)
+                console.log(`${message} Name is ${this.lobby.name}`)
+                console.log(`${message} Password is ${this.password}`)
+            } 
+
+            await this.lobby.setPassword("")
+            console.log(`${message} Name is ${this.lobby.name}`)
+
+            //events
+            this.lobby.on("playerJoined", (obj) => {
+                if (obj.player.user.isClient())
+                this.lobby.setHost("#" + obj.player.user.id);
+                this.players.push(obj.player.user.username)
+                console.log(`${message} Made ${obj.player.user.username} host`)
+                console.log(`${message} ${obj.player.user.username} joined!`)
+            });
+
+            this.lobby.on("playerLeft", (obj) => {
+                const index = this.players.indexOf(obj.user.username);
+                if (index > -1) {
+                    this.players.splice(index, 1);
+                }
+                console.log(`${message} ${obj.user.username} left!`)
+            })
+
+            this.lobby.on("matchFinished", () => {
+                this.players.push(this.players.shift());
+                this.lobby.setHost(this.players[0])
+                console.log(`${message} Made ${this.players[0]} host`)
+                this.channel.sendMessage(`Current host order; ${this.players.join(", ")}`)
+            });
+
+            this.lobby.on("beatmapId", async (id) => {
+                if (platform === 'linux') {
+                    exec(`curl https://osu.ppy.sh/osu/${id} | ./oppai - -ojson`, (error, stdout, stderr) => {
+                        if (!stdout) {
+                            return console.log(stderr)
+                        }
+                        const mapinfo = JSON.parse(stdout)
+
+                        this.channel.sendMessage(`[https://osu.ppy.sh/osu/${id} ${mapinfo.artist} - ${mapinfo.title}], MAX COMBO: ${mapinfo.max_combo} | ${mapinfo.pp.toFixed(0)}PP | ${mapinfo.stars.toFixed(2)}* AR${mapinfo.ar.toFixed(1)} CS${mapinfo.cs.toFixed(1)} HP${mapinfo.hp.toFixed(1)} OD${mapinfo.od.toFixed(1)} | Alternitave [https://beatconnect.io/b/${id} beatconnect.io]`)
+                    })
+                }
+
+                if (platform === 'win32') {
+                    exec(`powershell -command "(New-Object System.Net.WebClient).DownloadString('https://osu.ppy.sh/osu/${id}') | ./oppai - -ojson"`, (error, stdout, stderr) => {
+                        if (!stdout) {
+                            return console.log(stderr)
+                        }
+                        const mapinfo = JSON.parse(stdout)
+
+                        this.channel.sendMessage(`[https://osu.ppy.sh/osu/${id} ${mapinfo.artist} - ${mapinfo.title}], MAX COMBO: ${mapinfo.max_combo} | ${mapinfo.pp.toFixed(0)}PP | ${mapinfo.stars.toFixed(2)}* AR${mapinfo.ar.toFixed(1)} CS${mapinfo.cs.toFixed(1)} HP${mapinfo.hp.toFixed(1)} OD${mapinfo.od.toFixed(1)} | Alternitave [https://beatconnect.io/b/${id} beatconnect.io]`)
+                    })
+                }
+            })
+
+
+
+            this.channel.on("message", (msg) => {
+                if (msg.message === '-info') {
+                    this.channel.sendMessage('I am a auto host rotate bot.')
+                }
+
+                if (msg.message === '-queue') {
+                    this.channel.sendMessage(`Current queue; ${players.join(", ")}`)
+                }
+            })
+
+        }
+        run()
+    }
+}
+
+module.exports.autoHostRotate = autoHostRotate;
---
+++ index.js
@@ -0,0 +1,39 @@
+const Bancho = require("bancho.js");
+require('dotenv').config()
+const chalk = require("chalk");
+const { autoHostRotate } = require("./autoHostRotate.js")
+
+let message = chalk.cyan('[*]')
+let success = chalk.green('[!]')
+let warn = chalk.yellow('[!]')
+let danger = chalk.red('[!]')
+
+const exec = require('child_process').exec
+const os = require("os");
+const platform = os.platform();
+
+console.log(process.env.osuname, process.env.osupass)
+
+const client = new Bancho.BanchoClient({ 
+    username: process.env.osuname, 
+    password: process.env.osupass,
+    apiKey: process.env.apiKey,
+});
+
+const lobbies = []
+
+client.connect()
+    .then(() => {
+        lobbies.push(new autoHostRotate(client, "bot test 1", true, chalk));
+        lobbies.push(new autoHostRotate(client, "bot test 2", false, chalk));
+    })
+
+
+
+process.on("SIGINT", async () => {
+	console.log("Closing lobby and disconnecting...");
+    for(let i = 0; i < lobbies.length; i++) {
+        lobbies[i].lobby.closeLobby();
+    }
+	await client.disconnect();
+});
